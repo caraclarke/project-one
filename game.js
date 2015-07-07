e766ad7962ffbb9cbc25c42a081fae5ffbf7d8e2
caraclarke <cara.clarke8@gmail.com>
2015-07-06 21:18:18 -0400

    Event Tracking working, player switch working
    Event tracker for moves and wins working. Player switch working, css clean ups.

--- game.js
+++ game.js
@@ -27,12 +27,10 @@ function move(cell) {
     cellElement.innerHTML = turn;
     if (turn == "X") {
       turn = "O";
-      document.getElementById("playerTwo").innerHTML = "Your turn!";
-      document.getElementById("playerOne").innerHTML = "You are up next.";
+      addEventOutputTracking("Player Two", " - it is your turn. ", "Don't screw this up." + " X is up next.");
     } else {
       turn = "X";
-      document.getElementById("playerOne").innerHTML = "Your turn!";
-      document.getElementById("playerTwo").innerHTML = "You are up next.";
+      addEventOutputTracking("Player One", " - it is your turn. ", "Don't screw this up." + " O is up next.");
     }
     check();
   }
@@ -52,31 +50,40 @@ function check() {
 
   if (cell1 == cell2 && cell2 == cell3 && cell1 == cell3 && cell1 != "") {
     alert(cell1 + " Wins!");
+    addEventOutputTracking(cell1, " wins this round!", " Hooray.");
   } else if (cell4 == cell5 && cell5 == cell6 && cell4 == cell6 && cell4 != "") {
       alert(cell4 + " Wins!");
+      addEventOutputTracking(cell4, " wins this round!", " Hooray.");
     } else if (cell7 == cell8 && cell8 == cell9 && cell7 == cell9 && cell7 != "") {
         alert(cell7 + " Wins!");
+        addEventOutputTracking(cell7, " wins this round!", " Hooray.");
       } else if (cell1 == cell4 && cell4 == cell7 && cell1 == cell7 && cell1 != "") {
           alert(cell1 + " Wins!");
+          addEventOutputTracking(cell1, " wins this round!", " Hooray.");
         } else if (cell2 == cell5 && cell5 == cell8 && cell2 == cell8 && cell2 != "") {
             alert(cell2 + " Wins!");
+            addEventOutputTracking(cell2, " wins this round!", " Hooray.");
           } else if (cell3 == cell6 && cell6 == cell9 && cell3 == cell9 && cell3 != "") {
               alert(cell3 + " Wins!");
+              addEventOutputTracking(cell3, " wins this round!", " Hooray.");
             } else if (cell1 == cell5 && cell5 == cell9 && cell1 == cell9 && cell1 != "") {
                 alert(cell1 + " Wins!");
+                addEventOutputTracking(cell1, " wins this round!", " Hooray.");
               } else if (cell3 == cell5 && cell5 == cell7 && cell3 == cell7 && cell3 != "") {
                   alert(cell3 + " Wins!");
+                  addEventOutputTracking(cell3, " wins this round!", " Hooray.");
                 } else if (cell1 != "" && cell2 != "" && cell3 != "" && cell4 != "" && cell5 != "" && cell6 != "" && cell7 != "" && cell8 != "" && cell9 != "") {
                     alert("Tie!");
+                    addEventOutputTracking("Nobody wins!", " It's a cats game, ", " try harder next time.");
                   }
   }
 
 
-$('#clear').on('click', function() {
-  for (var i = 1; i < 10; i++) {
-    document.getElementById("gameboard").innerHTML = "";
-  }
-});
+// $('#clear').on('click', function() {
+//   for (var i = 1; i < 10; i++) {
+//     document.getElementById("gameboard").innerHTML = "";
+//   }
+// });
 
 function addEventOutputTracking (eventName, outputText, extraText) {
   var node;
@@ -84,5 +91,5 @@ function addEventOutputTracking (eventName, outputText, extraText) {
 
     node = document.getElementById("eventOutput");
     node.appendChild(pChildNode);
-    pChildNode.appendChild(document.createTextNode("User Event: " + eventName + " - " + outputText + " " + extraText));
+    pChildNode.appendChild(document.createTextNode("User Event: " + eventName + outputText + extraText));
   }
