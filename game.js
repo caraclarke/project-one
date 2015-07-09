58efd9ab05f3e2d5f74168678ac699109b13505c
caraclarke <cara.clarke8@gmail.com>
2015-07-08 20:32:28 -0400

    Gameboard working
    Fixed function got the gameboard to work!!

--- game.js
+++ game.js
@@ -5,28 +5,26 @@ $(document).ready( function () {
   getPlayers();
 
   $('#clear').on('click', function() {
-      $("td.cell").html("");
+    $("td.cell").html("");
   }); // end clear
 
   $("td.cell").on("click", function(cell){
-  var cellElement = $("td.cell").html();
-  var count = 0;
+    var cellElement = $("#cell");
 
-  if (cellElement != "X" && cellElement != "O") {
-    // cellElement = turn;
-
-    if (count % 2 !== 0) {
-      // turn = "X";
-      $(this).html("X");
-      ++count;
-      // addEventOutputTracking("Player Two", " - it is your turn. ", "Don't screw this up." + " X is up next.");
-    } else {
-      $(this).html("O");
-      ++count;
-      // addEventOutputTracking("Player One", " - it is your turn. ", "Don't screw this up." + " O is up next.");
+    if (cellElement.html() != "X" && cellElement.html() != "O"){
+      cellElement.html(turn);
+      if (turn == "X"){
+        $(this).html("X");
+        turn = "O";
+        addEventOutputTracking("Player Two", " - it is your turn. ", "Don't screw this up." + " X is up next.");
+      } else{
+        $(this).html("O");
+        turn = "X";
+        addEventOutputTracking("Player One", " - it is your turn. ", "Don't screw this up." + " O is up next.");
+      }
+      check();
     }
-    check();
-  }
+
 }); // end td
 
 }); // end document.ready
@@ -55,47 +53,47 @@ function check() {
   if (cell1 == cell2 && cell2 == cell3 && cell1 == cell3 && cell1 !== "") {
     alert(cell1 + " Wins!");
     console.log("winner");
-    // addEventOutputTracking(cell1, " wins this round!", " Hooray.");
+    addEventOutputTracking(cell1, " wins this round!", " Hooray.");
   } else if (cell4 == cell5 && cell5 == cell6 && cell4 == cell6 && cell4 !== "") {
     alert(cell4 + " Wins!");
     console.log("winner");
-    // addEventOutputTracking(cell4, " wins this round!", " Hooray.");
+    addEventOutputTracking(cell4, " wins this round!", " Hooray.");
   } else if (cell7 == cell8 && cell8 == cell9 && cell7 == cell9 && cell7 !== "") {
     alert(cell7 + " Wins!");
     console.log("winner");
-    // addEventOutputTracking(cell7, " wins this round!", " Hooray.");
+    addEventOutputTracking(cell7, " wins this round!", " Hooray.");
   } else if (cell1 == cell4 && cell4 == cell7 && cell1 == cell7 && cell1 !== "") {
     alert(cell1 + " Wins!");
     console.log("winner");
-    // addEventOutputTracking(cell1, " wins this round!", " Hooray.");
+    addEventOutputTracking(cell1, " wins this round!", " Hooray.");
   } else if (cell2 == cell5 && cell5 == cell8 && cell2 == cell8 && cell2 !== "") {
     alert(cell2 + " Wins!");
     console.log("winner");
-    // addEventOutputTracking(cell2, " wins this round!", " Hooray.");
+    addEventOutputTracking(cell2, " wins this round!", " Hooray.");
   } else if (cell3 == cell6 && cell6 == cell9 && cell3 == cell9 && cell3 !== "") {
     alert(cell3 + " Wins!");
     console.log("winner");
-    // addEventOutputTracking(cell3, " wins this round!", " Hooray.");
+    addEventOutputTracking(cell3, " wins this round!", " Hooray.");
   } else if (cell1 == cell5 && cell5 == cell9 && cell1 == cell9 && cell1 !== "") {
     alert(cell1 + " Wins!");
     console.log("winner");
-    // addEventOutputTracking(cell1, " wins this round!", " Hooray.");
+    addEventOutputTracking(cell1, " wins this round!", " Hooray.");
   } else if (cell3 == cell5 && cell5 == cell7 && cell3 == cell7 && cell3 !== "") {
     alert(cell3 + " Wins!");
     console.log("winner");
-    // addEventOutputTracking(cell3, " wins this round!", " Hooray.");
+    addEventOutputTracking(cell3, " wins this round!", " Hooray.");
   } else if (cell1 !== "" && cell2 !== "" && cell3 !== "" && cell4 !== "" && cell5 !== "" && cell6 !== "" && cell7 !== "" && cell8 !== "" && cell9 !== "") {
     alert("Tie!");
     console.log("winner");
-    // addEventOutputTracking("Nobody wins!", " It's a cats game, ", " try harder next time.");
+    addEventOutputTracking("Nobody wins!", " It's a cats game, ", " try harder next time.");
   }
 }
 
 function addEventOutputTracking (eventName, outputText, extraText) {
   var node;
-  var pChildNode = $("<p></p>").text; // .text
+  var pChildNode = document.createElement("p");
 
-  node = $("#eventOutput");
-  node.append(pChildNode);
-  pChildNode.append($.appendTo("User Event: " + eventName + outputText + extraText));
+  node = document.getElementById("eventOutput");
+  node.appendChild(pChildNode);
+  pChildNode.appendChild(document.createTextNode("User Event: " + eventName + outputText + extraText));
 }
