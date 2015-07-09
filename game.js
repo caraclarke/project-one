cf3368778357687e04ea31dadfc17f80748156a0
caraclarke <cara.clarke8@gmail.com>
2015-07-09 09:36:16 -0400

    Button updates, gameplay updates
    Updated random move to make sure it didn't overwrite previous moves. Created game history.

--- game.js
+++ game.js
@@ -31,28 +31,32 @@ $(document).ready( function () {
 
 $('#randomPlay').on('click', function() {
   var playRandom = Math.random();
+  var cellElement = $("#cell");
 
-  if (playRandom < 0.11) {
+  if (cellElement.html() != "X" && cellElement.html() != "O") {
+    if (playRandom < 0.11) {
     $("#4").html("O");
-  } else if (0.12 < playRandom < 0.2) {
-    $("#7").html("O");
-  } else if (0.2 < playRandom < 0.3) {
-    $("#3").html("X");
-  } else if (0.3 < playRandom < 0.4) {
-    $("#6").html("O");
-  } else if (0.4 < playRandom < 0.5) {
-    $("#8").html("O");
-  } else if (0.5 < playRandom < 0.6) {
-    alert("Don't be a pansy, just pick a square!");
-  } else if (0.6 < playRandom < 0.7) {
-    $("#9").html("X");
-  } else if (0.7 < playRandom < 0.8) {
-    $("#4").html("O");
-  } else if (0.8 < playRandom < 0.9) {
-    $("#2").html("X");
-  } else if (0.9 < playRandom < 1) {
-    $("#1").html("O");
-  } // end else if
+    } else if (0.12 < playRandom < 0.2) {
+      $("#7").html("O");
+    } else if (0.2 < playRandom < 0.3) {
+      $("#3").html("X");
+    } else if (0.3 < playRandom < 0.4) {
+      $("#6").html("O");
+    } else if (0.4 < playRandom < 0.5) {
+      $("#8").html("O");
+    } else if (0.5 < playRandom < 0.6) {
+      alert("Don't be a pansy, just pick a square!");
+    } else if (0.6 < playRandom < 0.7) {
+      $("#9").html("X");
+    } else if (0.7 < playRandom < 0.8) {
+      $("#4").html("O");
+    } else if (0.8 < playRandom < 0.9) {
+      $("#2").html("X");
+    } else if (0.9 < playRandom < 1) {
+      $("#1").html("O");
+    } // end else if
+    check();
+  }
 
 }); // end randomPlay
 
@@ -94,32 +98,40 @@ function check() {
 
   if (cell1 == cell2 && cell2 == cell3 && cell1 == cell3 && cell1 !== "") {
     alert(cell1 + " Wins!");
-    addEventOutputTracking(cell1, " wins this round!", " Hooray.");
-    // gameHistory(cell1, " wins this round!", " Hooray.");
+      addEventOutputTracking(cell1, " wins this round!", " Hooray.");
+      gameHistory(cell1, " wins this round!", " Hooray.");
   } else if (cell4 == cell5 && cell5 == cell6 && cell4 == cell6 && cell4 !== "") {
     alert(cell4 + " Wins!");
-    addEventOutputTracking(cell4, " wins this round!", " Hooray.");
+      addEventOutputTracking(cell4, " wins this round!", " Hooray.");
+      gameHistory(cell4, " wins this round!", " Hooray.");
   } else if (cell7 == cell8 && cell8 == cell9 && cell7 == cell9 && cell7 !== "") {
     alert(cell7 + " Wins!");
-    addEventOutputTracking(cell7, " wins this round!", " Hooray.");
+      addEventOutputTracking(cell7, " wins this round!", " Hooray.");
+      gameHistory(cell7, " wins this round!", " Hooray.");
   } else if (cell1 == cell4 && cell4 == cell7 && cell1 == cell7 && cell1 !== "") {
     alert(cell1 + " Wins!");
-    addEventOutputTracking(cell1, " wins this round!", " Hooray.");
+      addEventOutputTracking(cell1, " wins this round!", " Hooray.");
+      gameHistory(cell1, " wins this round!", " Hooray.");
   } else if (cell2 == cell5 && cell5 == cell8 && cell2 == cell8 && cell2 !== "") {
     alert(cell2 + " Wins!");
-    addEventOutputTracking(cell2, " wins this round!", " Hooray.");
+      addEventOutputTracking(cell2, " wins this round!", " Hooray.");
+      gameHistory(cell2, " wins this round!", " Hooray.");
   } else if (cell3 == cell6 && cell6 == cell9 && cell3 == cell9 && cell3 !== "") {
     alert(cell3 + " Wins!");
-    addEventOutputTracking(cell3, " wins this round!", " Hooray.");
+      addEventOutputTracking(cell3, " wins this round!", " Hooray.");
+      gameHistory(cell3, " wins this round!", " Hooray.");
   } else if (cell1 == cell5 && cell5 == cell9 && cell1 == cell9 && cell1 !== "") {
     alert(cell1 + " Wins!");
-    addEventOutputTracking(cell1, " wins this round!", " Hooray.");
+      addEventOutputTracking(cell1, " wins this round!", " Hooray.");
+      gameHistory(cell1, " wins this round!", " Hooray.");
   } else if (cell3 == cell5 && cell5 == cell7 && cell3 == cell7 && cell3 !== "") {
     alert(cell3 + " Wins!");
-    addEventOutputTracking(cell3, " wins this round!", " Hooray.");
+      addEventOutputTracking(cell3, " wins this round!", " Hooray.");
+      gameHistory(cell3, " wins this round!", " Hooray.");
   } else if (cell1 !== "" && cell2 !== "" && cell3 !== "" && cell4 !== "" && cell5 !== "" && cell6 !== "" && cell7 !== "" && cell8 !== "" && cell9 !== "") {
     alert("Tie!");
-    addEventOutputTracking("Nobody wins!", " It's a cats game, ", " try harder next time.");
+      addEventOutputTracking("Nobody wins!", " It's a cats game, ", " try harder next time.");
+      gameHistory("Nobody", " wins this round!", " Get it the fuck together.");
   }
 }
 
@@ -132,11 +144,11 @@ function addEventOutputTracking (eventName, outputText, extraText) {
   pChildNode.appendChild(document.createTextNode("User Event: " + eventName + outputText + extraText));
 } // end AEOT
 
-function gameHistory (eventName, player, outputText) {
+function gameHistory (winner, outputText) {
   var node;
   var pChildNode = document.createElement("p");
 
   node = document.getElementById("playerHistory");
   node.appendChild(pChildNode);
-  pChildNode.appendChild(document.createTextNode("Winner " + player + outputText));
+  pChildNode.appendChild(document.createTextNode(winner + outputText));
 } // end GH
